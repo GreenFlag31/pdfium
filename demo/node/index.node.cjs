543bad37488f1d05cbc5b06ed8536a4deaf3e309
unknown <unam31@gmail.com>
2026-02-19 20:35:42 +0100

    double quotes in demo files

--- demo/node/index.node.cjs
+++ demo/node/index.node.cjs
@@ -1,26 +1,26 @@
-const { PDFiumLibrary } = require('@hyzyla/pdfium');
-const fs = require('fs');
+const { PDFiumLibrary } = require("@hyzyla/pdfium");
+const fs = require("fs");
 
 async function testPdfiumPackage() {
   const library = await PDFiumLibrary.init();
-  console.log('=> library initialized');
+  console.log("=> library initialized");
 
-  const pdf = fs.readFileSync('sample.pdf');
+  const pdf = fs.readFileSync("sample.pdf");
   const doc = await library.loadDocument(pdf);
-  console.log('=> document loaded');
-  console.log('===> number of pages:', doc.getPageCount());
+  console.log("=> document loaded");
+  console.log("===> number of pages:", doc.getPageCount());
 
-  const page = await doc.getPage(0);
-  console.log('=> page loaded');
+  const page = doc.getPage(0); // not async
+  console.log("=> page loaded");
 
   const pageSize = page.getOriginalSize();
-  console.log('===> page size:', pageSize.originalWidth, 'x', pageSize.originalHeight);
+  console.log("===> page size:", pageSize.originalWidth, "x", pageSize.originalHeight);
 
   doc.destroy();
-  console.log('=> document destroyed');
+  console.log("=> document destroyed");
 
   library.destroy();
-  console.log('=> library destroyed');
+  console.log("=> library destroyed");
 }
 
 testPdfiumPackage();
--- demo/node/index.node.mjs
+++ demo/node/index.node.mjs
@@ -1,26 +1,26 @@
-import { PDFiumLibrary } from '@hyzyla/pdfium';
-import fs from 'fs';
+import { PDFiumLibrary } from "@hyzyla/pdfium";
+import fs from "fs";
 
 async function testPdfiumPackage() {
   const library = await PDFiumLibrary.init();
-  console.log('=> library initialized');
+  console.log("=> library initialized");
 
-  const pdf = fs.readFileSync('sample.pdf');
+  const pdf = fs.readFileSync("sample.pdf");
   const doc = await library.loadDocument(pdf);
-  console.log('=> document loaded');
-  console.log('===> number of pages:', doc.getPageCount());
+  console.log("=> document loaded");
+  console.log("===> number of pages:", doc.getPageCount());
 
-  const page = await doc.getPage(0);
-  console.log('=> page loaded');
+  const page = doc.getPage(0); // not async
+  console.log("=> page loaded");
 
   const pageSize = page.getOriginalSize();
-  console.log('===> page size:', pageSize.originalWidth, 'x', pageSize.originalHeight);
+  console.log("===> page size:", pageSize.originalWidth, "x", pageSize.originalHeight);
 
   doc.destroy();
-  console.log('=> document destroyed');
+  console.log("=> document destroyed");
 
   library.destroy();
-  console.log('=> library destroyed');
+  console.log("=> library destroyed");
 }
 
 testPdfiumPackage();
